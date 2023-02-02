b8f8dc9e6e0a5666f0408a5e53c4147fc58445ad
Anton Kutsim <kutsimanton@gmail.com>
2023-02-02 20:29:10 +0200

    start

---
+++ portfolio-app/src/App.js
@@ -0,0 +1,16 @@
+import { Header } from "./components/Header/Header";
+import { CatalogList } from "./components/CatalogList";
+import { Hero } from "./components/Hero/Hero";
+import { Footer } from "./components/Footer";
+function App() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <CatalogList />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ portfolio-app/src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+export const Header = () => {
+    return (
+        <>
+            <div>
+                <h2>Hello you</h2>
+            </div>
+        </>
+    );
+}
